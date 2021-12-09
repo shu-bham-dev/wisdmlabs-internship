30a96b980c1568a3078951f38f594cf29d93571c
Shubham <ss.cse1001@gmail.com>
2021-12-09 11:28:42 +0530

    This is my first file of learning php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorial 1</title>
+</head>
+<body>
+    This is the first PHP tutorial.
+    <?php
+    echo "<br>Hello i am into php"
+    ?>
+</body>
+</html>
