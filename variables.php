e2b3a8017e436d14bb75f8855b0fff1a2a39d85b
Shubham <ss.cse1001@gmail.com>
2021-12-09 12:00:15 +0530

    Added variabled file

---
+++ variables.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Variables</title>
+</head>
+<body>
+    <?php
+    $var1 = "Shubham<br>";
+    $var2 = 22;
+    $var3 = 22.8;
+    echo $var1;
+    echo $var2;
+
+    echo "<br>";
+    echo var_dump(4==4);
+    echo "<br>";
+    echo var_dump($var1);
+    echo "<br>";
+    echo var_dump($var2);    
+    echo "<br>";
+    echo var_dump($var3);
+    ?>
+</body>
+</html>
